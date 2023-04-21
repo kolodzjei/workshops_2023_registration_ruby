fd5a34e25936183d3fb2360a45abcbe424f74739
Dawid <kolodziejdawid02@gmail.com>
2023-04-21 16:11:26 +0200

    Add solutions

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,9 @@
 class CountElements
+  def initialize(results)
+    @results = results
+  end
 
+  def perform
+    @results.tally
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,9 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    @password =~ /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%&*+=:;?<>])(?!.*(.)\1{2,})([\S]){6,24}$/ ? true : false
+  end
 end
